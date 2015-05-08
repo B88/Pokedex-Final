bd877222163147e9b384f810aabfc4a36ac8e9f8
Gregg <gmcmillion215@gmail.com>
2015-05-08 11:08:06 -0700

    Added 2 intqueue files

---
+++ IntQueue.h
@@ -0,0 +1,27 @@
+//Specification file for the IntQueue Class
+#ifndef INTQUEUE_H
+#define INTQUEUE_H
+
+class IntQueue
+{
+    private:
+        int *queueArray;        //Points to the queue array
+        int queueSize;          //Variable that holds the queue size
+        int front;              //Subscript of the queue front
+        int rear;               //Subscript to the queue rear
+        int numItems;           //Number of items in the queue
+    public:
+        //Constructor
+        IntQueue(int);
+    
+        //Copy Constructor
+        IntQueue(const IntQueue &);
+    
+        //Queue Operations
+        void enqueue(int);      //To add to the queue
+        void dequeue(int &);    //To take first in the queue
+        bool isEmpty() const;   //To check if queue is empty
+        bool isFull() const;    //To check if queue is full
+        void clear();           //To clear the queue
+};
+#endif
